b0cd29df29218717903bece8f0ed774df1da058d
lostlang <53492781+lostlang@users.noreply.github.com>
2021-11-07 15:50:06 +0300

    Controller complete

--- spring-boot/src/main/kotlin/com/example/demo/controller/Controller.kt
+++ spring-boot/src/main/kotlin/com/example/demo/controller/Controller.kt
@@ -1,3 +1,11 @@
 package com.example.demo.controller
 
-class Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+    @GetMapping("/{value}")
+    fun echo(@PathVariable value: String) = value
+}
